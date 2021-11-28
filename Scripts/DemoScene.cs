9f37da716d3af8b259a3f4e903adb28b4e72023c
Adib 444034 <62319714+pihdastudios@users.noreply.github.com>
2021-11-28 20:38:58 +0700

    update

--- Scripts/DemoScene.cs
+++ Scripts/DemoScene.cs
@@ -1,24 +1,24 @@
 using Godot;
 using System;
-using GodotOnReady.Attributes;
 
 public partial class DemoScene : Spatial
 {
-    [OnReadyGet("Player1Pos")] private Position3D player1Pos;
-    [OnReadyGet("Player2Pos")] private Position3D player2Pos;
+    private Position3D player1Pos;
+    private Position3D player2Pos;
     
-    private PackedScene player1Scn = GD.Load<PackedScene>("res://Scenes/Player/Player.tscn");
-    private PackedScene player2Scn = GD.Load<PackedScene>("res://Scenes/Player/Player.tscn");
+    private PackedScene playerScn= GD.Load<PackedScene>("res://Scenes/Player/Player.tscn");
 
 
     public override void _Ready()
     {
-        var player1 = player1Scn.Instance<KinematicBody>();
+        player1Pos = GetNode<Position3D>("Player1Pos");
+        player2Pos = GetNode<Position3D>("Player2Pos");
+        var player1 = playerScn.Instance<KinematicBody>();
         player1.Name = GetTree().GetNetworkUniqueId().ToString();
         AddChild(player1);
         player1.GlobalTransform = player1Pos.GlobalTransform;
         
-        var player2 = player2Scn.Instance<KinematicBody>();
+        var player2 = playerScn.Instance<KinematicBody>();
         player2.Name = Globals.Player2Id.ToString();
         AddChild(player2);
         player2.GlobalTransform = player2Pos.GlobalTransform;
