311ecd4b0c7bf3fb73cf76cce5a48e051e327369
Adib 444034 <62319714+pihdastudios@users.noreply.github.com>
2021-12-02 09:28:43 +0700

    add label

--- Scripts/Interface/Hud.cs
+++ Scripts/Interface/Hud.cs
@@ -4,8 +4,20 @@ using System;
 public class Hud : CanvasLayer
 {
     private Label interactionLbl;
+    private Label gameOverLbl;
     public override void _Ready()
     {
-        interactionLbl = GetNode<Label>("CenterContainer/InteractionLabel");
+        interactionLbl = GetNode<Label>("CenterContainer/VBoxContainer/InteractionLabel");
+        gameOverLbl = GetNode<Label>("CenterContainer/VBoxContainer/GameOverLabel");
+    }
+
+    public void Win()
+    {
+        gameOverLbl.Text = "You Win";
+    }
+    
+    public void Loose()
+    {
+        gameOverLbl.Text = "You Lost";
     }
 }
